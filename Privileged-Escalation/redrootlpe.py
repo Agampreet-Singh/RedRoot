4b2e7839c763e7c77b769116cf050479e65ad2d3
Agampreet Singh <73707055+Agampreet-Singh@users.noreply.github.com>
2025-07-29 22:18:34 +0530

    Create redrootlpe.py

---
+++ Privileged-Escalation/redrootlpe.py
@@ -0,0 +1,18 @@
+def locked_module():
+    banner = """
+    ==========================================
+            ⚠️  MODULE LOCKED ⚠️
+    ==========================================
+    This feature is not available in RedRoot Lite.
+    
+    👉 To access this module, please contact:
+       🔒 My Sir, Mr. Agampreet
+       
+    🔗 IG: @th3_redr00t
+    ==========================================
+    """
+    print(banner)
+    exit()
+
+if __name__ == "__main__":
+    locked_module()
